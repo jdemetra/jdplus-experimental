7712a5f07c45845417860e14a2f5ea7c9315cdea
Jean Palate <palatej@users.noreply.github.com>
2024-01-02 18:31:19 +0100

    advanced toolkit (new project)

---
+++ jdplus-experimental-base/jdplus-toolkitx-base-parent/jdplus-toolkitx-base-core/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module jdplus.toolkitx.base.core {
+
+    requires static lombok;
+    requires static nbbrd.design;
+    requires static nbbrd.service;
+    requires static org.checkerframework.checker.qual;
+
+    requires jdplus.toolkit.base.api;
+    requires jdplus.toolkit.base.core;
+
+}
---
+++ jdplus-experimental-base/jdplus-toolkitx-base-parent/jdplus-toolkitx-base-r/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module jdplus.toolkitx.base.r {
+
+    requires static lombok;
+    requires static nbbrd.design;
+    requires static nbbrd.service;
+    requires static org.checkerframework.checker.qual;
+
+    requires jdplus.toolkit.base.api;
+    requires jdplus.toolkit.base.core;
+
+    exports jdplus.toolkitx.base.r;
+
+}
